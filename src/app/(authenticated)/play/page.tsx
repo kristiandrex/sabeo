2ae4cb1d757d794b374472d35aa5bad58fd89395
Cristian Torrez <cristiandrestorres@gmail.com>
2024-12-29 20:24:35 -0500

    feat: add reload button

--- src/app/(authenticated)/play/page.tsx
+++ src/app/(authenticated)/play/page.tsx
@@ -1,8 +1,11 @@
-import { Game } from "#/components/game";
+import { Button } from "@radix-ui/themes";
+import { revalidatePath } from "next/cache";
+
 import {
   getAttemptsByPlayer,
   getLatestChallenge,
 } from "#/app/actions/challenge";
+import { Game } from "#/components/game";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
@@ -10,10 +13,16 @@ export const revalidate = 0;
 export default async function PlayPage() {
   const latestChallenge = await getLatestChallenge();
 
+  async function reload() {
+    "use server";
+    revalidatePath("/play");
+  }
+
   if (!latestChallenge) {
     return (
-      <div className="flex h-screen items-center justify-center">
+      <div className="flex h-screen items-center justify-center flex-col gap-2">
         <h1 className="text-2xl font-semibold">No hay reto disponible</h1>
+        <Button onClick={reload}>Recargar</Button>
       </div>
     );
   }
