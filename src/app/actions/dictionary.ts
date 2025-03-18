99c4392546e66179dca837fd05fa45298ec058b9
Cristian Torrez <cristiandrestorres@gmail.com>
2025-03-17 20:19:19 -0500

    feat: handle read dictionary error

--- src/app/actions/dictionary.ts
+++ src/app/actions/dictionary.ts
@@ -5,16 +5,28 @@ import path from "node:path";
 
 import { cache } from "react";
 
-export const getDictionary = cache(async function getDictionary() {
-  const dictionaryPath = path.join(
-    process.cwd(),
-    "src",
-    "app",
-    "assets",
-    "dictionary.json"
-  );
+export const getDictionary = cache(async function getDictionary(): Promise<{
+  aff: string;
+  dic: string;
+}> {
+  try {
+    const dictionaryPath = path.join(
+      process.cwd(),
+      "src",
+      "app",
+      "assets",
+      "dictionary.json"
+    );
 
-  const content = await fs.readFile(dictionaryPath, "utf8");
-  const dictionary = JSON.parse(content);
-  return dictionary;
+    const content = await fs.readFile(dictionaryPath, "utf8");
+    const dictionary = JSON.parse(content);
+    return dictionary;
+  } catch (error) {
+    console.error(error);
+
+    return {
+      aff: "",
+      dic: "",
+    };
+  }
 });
