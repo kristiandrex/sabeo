7255c74b539ceaacdc05cea2bda8ba3b4a6be31e
Cristian Torrez <cristiandrestorres@gmail.com>
2025-11-16 00:53:43 -0500

    Comment out pgsodium extension in migration

--- supabase/migrations/20250221010422_remote_schema.sql
+++ supabase/migrations/20250221010422_remote_schema.sql
@@ -12,7 +12,8 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
-CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+-- CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+-- See https://github.com/supabase/cli/issues/3358#issuecomment-2755803583
 
 
 
